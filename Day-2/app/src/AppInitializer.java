c6fe5c59a26a9c30402c98961f101b8ac7d537e0
Osagani31 <naonperera@students.nsbm.ac.lk>
2026-04-02 18:36:56 +0530

    Day 2 -Activities

---
+++ Day-2/app/src/AppInitializer.java
@@ -0,0 +1,31 @@
+import entity.Laptop;
+import entity.Student;
+import org.hibernate.Session;
+import org.hibernate.Transaction;
+
+public class AppInitializer {
+    public static void main(String[] args) {
+        Laptop laptop = new Laptop(1,"Lenovo");
+        Student student1 = new Student(1,"Kamal",laptop);
+        saveStudent(student1);
+        //findStudent(1);
+    }
+
+    private static void findStudent(long id) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Student selectedStudent = session.find(Student.class, id);
+            System.out.println(selectedStudent);
+        }
+
+    }
+
+    private static void saveStudent(Student student) {
+        try (Session session = HibernateUtil.getSessionFactory().openSession()) {
+            Transaction transaction = session.beginTransaction();
+            long savedId = (long) session.save(student);
+            transaction.commit();
+            System.out.println("saved Student Primary Key : " + savedId);
+        }
+    }
+
+}
